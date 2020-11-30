2b36ffe2ffb4aeb3ff6d3659d2369d32dc228850
Michel de Sousa <michelgdesousa@gmail.com>
2020-11-29 23:55:19 -0300

    alterações quase finais

---
+++ frontend/src/pages/RedirectPage/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Header from '../../components/Header';
+import { Container } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import ShortenerService from '../../services/shortenerService';
+
+class RedirectPage extends React.Component {
+    constructor( props ) {
+        super( props );
+
+        this.state = {
+            isLoading: false,
+            url: '',
+            errorMessage: '';
+        }
+    }
+
+    render() {
+        return (
+          <Container>
+              <Header>Redirecionando...</Header>
+          </Container>
+        )
+    }
+}
+
+export default RedirectPage;
